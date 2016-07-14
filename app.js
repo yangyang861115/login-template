196458c857c81d7adfae0d5c29956da1e4f0a5d6
yangyang861115 <yangyang861115@gmail.com>
2016-07-14 14:03:19 -0700

    upload user photo

---
+++ app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by yangyang on 3/31/16.
+ */
+(function(){
+    angular
+        .module("myApp", ['ngRoute','ngStorage', 'ui.bootstrap', 'ui.validate', 'ngFileUpload', 'ngImgCrop']);
+})();
